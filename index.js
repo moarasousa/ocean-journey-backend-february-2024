eae8b40f3280270bde4d9f6d330281836b97d762
Moara Sousa <139187714+moarasousa@users.noreply.github.com>
2024-02-21 14:42:24 +0000

    Endpoint [GET] /item/:id created

--- index.js
+++ index.js
@@ -17,4 +17,16 @@ app.get('/oi', function (req, res) {
     res.send(lista)
   })
 
+  // Read By ID -> [GET] /item/:id
+  app.get('/item/:id', function (req, res) {
+    //Acesso o ID no parâmetro de rota
+    const id = req.params.id
+
+    //Acesso item na lista baseado no ID recebido
+    const item = lista[id]
+
+    //Envio o item obtido como resposta HTTP
+    res.send(item)
+  })
+
 app.listen(3000)
