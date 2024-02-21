0c170bc203646ce25c7500d731b8fd55a6216967
Moara Sousa <139187714+moarasousa@users.noreply.github.com>
2024-02-21 14:24:56 +0000

    Endpoint [GET] /item created

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ app.get('/oi', function (req, res) {
     res.send('Olá, Mundo')
   })
 
+  //Lista de Personagens
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+  //Read All -> [GET] /item
+  app.get('/item', function (req, res){
+    res.send(lista)
+  })
+
 app.listen(3000)
