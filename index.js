09cc7c63179fe65752776d8bad6610ae1748f879
Moara Sousa <139187714+moarasousa@users.noreply.github.com>
2024-02-28 14:45:53 +0000

    dotenv configuration

--- index.js
+++ index.js
@@ -28,26 +28,23 @@ async function main() {
   const db = client.db(dbName)
   const collection = db.collection('item')
 
-
   // Read All -> [GET] /items
   app.get('/items', async function (req, res) {
     // Realizamos a operação de find na collection do MongoDB
     const item = await collection.find().toArray()
-
     // Envio todos os documentos como resposta HTTP
     res.send(item)
   })
+
 
   // Read By ID -> [GET] /items/:id
   app.get('/items/:id', async function (req, res) {
     //Acesso o ID no parâmetro de rota
     const id = req.params.id
-
      // Acesso o items na collection baseado no ID recebido
      const items = await collection.findOne({
       _id: new ObjectId(id)
     })
-
     // Envio o items obtido como resposta HTTP
     res.send(items)
   })
@@ -55,43 +52,38 @@ async function main() {
   // Sinalizamos que o corpo da requisição está em JSON
   app.use(express.json())
 
+
   // Create -> [POST] /items
   app.post('/items', async function (req, res) {
     // Extraímos o corpo da requisição
     const items = req.body
-
     // Colocamos o item dentro da lista de itens
    await collection.insertOne(items)
-
     // Enviamos uma resposta de sucesso
     res.send(items)
   })
+
 
   // Uptade -> [PUT] /items/:id
   app.put('/items/:id', async function (req, res){
     // Pegamos o ID recebido pela rota
     const id = req.params.id
-
     // Pegamos o novo items do corpo da requisição
     const novoItems = req.body
-
     //Atualizamos o documento na collection
     collection.updateOne(
       { _id: new ObjectId(id)},
       { $set: novoItems}
       )
-
     res.send('Items atualizado com sucesso')})
+
 
     // Delete -> [DELETE] /items/:id
     app.delete('/items/:id', async function (req, res){
-
       // Pegamos o ID da rota
       const id = req.params.id
-
       // Realizamos a operação de deleteOne
       await cpllection.deleteOne({ _id: new ObjectId(id)})
-      
       //Enviamos uma mensagem de sucesso
       res.send('Item removido com sucesso')
       })
